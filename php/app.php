c96a86951aee70432b3cd2ee414852e37a662327
David Amanshia <phpengine@pharaohtools.com>
2016-11-06 09:38:03 +0000

    dynamically setting height based on content size

--- php/app.php
+++ php/app.php
@@ -13,6 +13,12 @@ require_once "php/src/UsingNamespaces/app.php" ;
 require_once "php/src/CountCurrentElements/app.php" ;
 require_once "php/src/Demo/app.php" ;
 
+$entries = $jQuery('div.demo_entry')->length ;
+$console->log("ent", $entries);
+$full_height = $entries * 130 ;
+$console->log("full height", $full_height, "ent", $entries);
+$jQuery('div#homepage_slider')->css("height", $full_height) ;
+
 $fade_in_closure = function() use ($jQuery) {
     $jQuery('div#homepage_loading')->slideUp("slow") ;
 } ;
