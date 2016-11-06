1cfa1b85a5bc589570c49ee84d8e4634ec3690e7
David Amanshia <phpengine@pharaohtools.com>
2016-11-06 07:10:18 +0000

    autoload register in parent app file

--- php/src/UsingNamespaces/app.php
+++ php/src/UsingNamespaces/app.php
@@ -4,16 +4,11 @@ namespace UsingNamespaces;
 
 use UsingNamespaces\Logic ;
 
-spl_autoload_register(function ($class) {
-    $class = 'php/src/' . str_replace("\\", '/', $class) . '.php';
-    require_once $class;
-});
-
 Logic\Math::$math = $math ;
 
-$closure = function() use ($jQuery){
+$closure = function() use ($jQuery) {
     $random_number = Logic\Math::$math->floor((Logic\Math::$math->random() * 10) + 1);
-    $jQuery('')->css('background-color', 'blue') ;
+    $jQuery('div#display_random')->html($random_number) ;
 };
 
 $jQuery('body#using_namespaces')->on('click', $closure);
