c5c7c2a0fe7b344f0222a2e1fc29cb7bd27e96da
David Amanshia <phpengine@pharaohtools.com>
2016-11-06 21:47:04 +0000

    lorem ipsum not logic

--- php/src/UsingNamespaces/app.php
+++ php/src/UsingNamespaces/app.php
@@ -2,12 +2,13 @@
 
 namespace UsingNamespaces;
 
-use UsingNamespaces\Logic ;
+use UsingNamespaces\Lorem ;
 
 $using_namespaces_closure = function() use ($jQuery, $jsMath) {
+    $loremIpsum = new Lorem\Ipsum() ;
     $larger = $jsMath->random() * 10 ;
     $endRand = $jsMath->floor($larger + 1) ;
-    $jQuery('h2#counter')->html($endRand);
+    $jQuery('h2#counter')->html($loremIpsum->options[$endRand]);
 };
 
 $jQuery('div#using_namespaces_button')->on('click', $using_namespaces_closure);
