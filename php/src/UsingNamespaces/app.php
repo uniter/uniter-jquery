b23116a5306f5e86787278f7ff183828ba00e888
David Amanshia <phpengine@pharaohtools.com>
2016-11-06 08:28:26 +0000

    namespace using math better

--- php/src/UsingNamespaces/app.php
+++ php/src/UsingNamespaces/app.php
@@ -4,11 +4,9 @@ namespace UsingNamespaces;
 
 use UsingNamespaces\Logic ;
 
-Logic\Math::$math = $math ;
-
-$closure = function() use ($jQuery) {
-    $random_number = Logic\Math::$math->floor((Logic\Math::$math->random() * 10) + 1);
+$closure = function() use ($jQuery, $jsMath) {
+    $random_number = $jsMath->math->floor(($jsMath->math->random() * 10) + 1);
     $jQuery('div#display_random')->html($random_number) ;
 };
 
-$jQuery('body#using_namespaces')->on('click', $closure);
+$jQuery('h2#using_namespaces')->on('click', $closure);
