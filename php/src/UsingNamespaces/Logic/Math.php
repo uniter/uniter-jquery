d6f421c6e8e6ac11f7eb55a770c6379cfd345f7e
David Amanshia <phpengine@pharaohtools.com>
2016-11-06 21:46:09 +0000

    logic to lorem ipsum

--- php/src/UsingNamespaces/Logic/Math.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace UsingNamespaces\Logic ;
-
-class Math {
-
-    public static $math ;
-
-}
---
+++ php/src/UsingNamespaces/Lorem/Ipsum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace UsingNamespaces\Person ;
+
+class Food {
+
+    public $options = array(
+        "cow", "sheep", "chickens", "horse", "pig", "eggs", "duck", "fish", "chips", "quail", "mushrooms"
+    )  ;
+
+}
