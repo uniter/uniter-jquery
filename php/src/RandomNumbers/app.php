e6902deb3c3041b4c4f40f39710de51d3c3b72e5
David Amanshia <phpengine@pharaohtools.com>
2016-11-06 12:21:37 +0000

    random numbers works

--- php/src/RandomNumbers/app.php
+++ php/src/RandomNumbers/app.php
@@ -1,35 +1,9 @@
 <?php
 
-
-function update_counter($jQuery) {
-    $count = $jQuery('.demo_standard_entry')->length ;
-    $jQuery('h2#counter')->html($count);
-}
-
-function get_demo_element_html() {
-    $html = '
-    <div class="demo_entry demo_standard_entry">
-        A demo entry
-    </div>
-    ';
-    return $html ;
-}
-
-$add_element_closure = function() use ($jQuery, $console){
-    $console->log("something to say 1") ;
-    $new_el = get_demo_element_html() ;
-    $console->log("something to say 2", $new_el) ;
-    $jQuery('#demo_element_list')->append($new_el) ;
-    $console->log("something to say 3") ;
-    update_counter($jQuery);
-    $console->log("something to say 4") ;
+$select_random_closure = function() use ($jQuery, $jsMath, $console){
+    $larger = $jsMath->random() * 10 ;
+    $endRand = $jsMath->floor($larger + 1) ;
+    $jQuery('h2#counter')->html($endRand);
 };
 
-$remove_element_closure = function() use ($jQuery){
-    $last_el = $jQuery('.demo_standard_entry')->last() ;
-    $last_el->remove() ;
-    update_counter($jQuery);
-};
-
-$jQuery('div#add_element')->on('click', $add_element_closure);
-$jQuery('div#remove_element')->on('click', $remove_element_closure);
+$jQuery('div#random_select_button')->on('click', $select_random_closure);
