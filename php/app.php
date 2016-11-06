ab0fd04a6afb08b063f7ee79274df39eb57dcd02
David Amanshia <phpengine@pharaohtools.com>
2016-11-06 12:40:51 +0000

    streamlined not used

--- php/app.php
+++ php/app.php
@@ -1,10 +1,5 @@
 <?php
 
-spl_autoload_register(function ($class) {
-    $class = 'php/src/' . str_replace("\\", '/', $class) . '.php';
-    require_once $class;
-});
-
 require_once "php/src/HelloWorld/app.php" ;
 require_once "php/src/JQueryColourDemo/app.php" ;
 require_once "php/src/WindowAlertDemo/app.php" ;
@@ -17,9 +12,7 @@ require_once "php/src/Slides/app.php" ;
 require_once "php/src/Demo/app.php" ;
 
 $entries = $jQuery('div.demo_entry')->length ;
-$console->log("ent", $entries);
 $full_height = $entries * 130 ;
-$console->log("full height", $full_height, "ent", $entries);
 $jQuery('div#homepage_slider')->css("height", $full_height) ;
 
 $fade_in_closure = function() use ($jQuery) {
