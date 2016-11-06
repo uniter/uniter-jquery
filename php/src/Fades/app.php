02cb2b38a965ef49377fa5254bc8d8170f44b05f
David Amanshia <phpengine@pharaohtools.com>
2016-11-06 13:10:28 +0000

    layout, fade item under

--- php/src/Fades/app.php
+++ php/src/Fades/app.php
@@ -1,21 +1,12 @@
 <?php
 
-
-function update_counter($jQuery, $count) {
-    $jQuery('h2#counter')->html($count);
-}
-
-$select_random_closure = function() use ($jQuery, $jsMath, $console){
-    $console->log("something to say 1") ;
-    $standard_rand = $jsMath->random() ;
-    $console->log("something to say 2", $standard_rand) ;
-    $larger = $jsMath->random() * 10 ;
-    $console->log("something to say 3, $larger") ;
-    $endRand = $jsMath->floor($larger + 1) ;
-    $console->log("something to say 4, $endRand") ;
-    update_counter($jQuery, $endRand);
-    $console->log("something to say 5, $endRand") ;
+$fade_in_closure = function() use ($jQuery){
+    $jQuery('div#fade_target')->fadeIn("slow") ;
 };
 
+$fade_out_closure = function() use ($jQuery){
+    $jQuery('div#fade_target')->fadeOut("slow") ;
+};
 
-$jQuery('div#random_select_button')->on('click', $select_random_closure);
+$jQuery('div#fade_in_button')->on('click', $fade_in_closure);
+$jQuery('div#fade_out_button')->on('click', $fade_out_closure);
