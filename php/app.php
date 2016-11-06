cd0452f5a2605d7ca5165f4c7a3147c4834f1092
David Amanshia <phpengine@pharaohtools.com>
2016-11-06 06:43:02 +0000

    including not instantiating in app

--- php/app.php
+++ php/app.php
@@ -1,23 +1,13 @@
 <?php
 
-namespace Demo;
-
-use Demo\Component\NavMenuComponent;
-
 spl_autoload_register(function ($class) {
     $class = 'php/src/' . str_replace("\\", '/', $class) . '.php';
-
     require_once $class;
 });
 
 require_once "php/src/JQueryColourDemo/app.php" ;
 require_once "php/src/WindowAlertDemo/app.php" ;
 require_once "php/src/CountCurrentElements/app.php" ;
+require_once "php/src/Demo/app.php" ;
 
-$navMenu = new NavMenuComponent(
-    $jQuery,
-    $jQuery('body'),
-    $jQuery('.primary-nav')
-);
-
-$navMenu->initialize();
+$jQuery('div#homepage_slider')->slideDown('slow');
